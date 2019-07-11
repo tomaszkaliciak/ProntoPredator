68086c3da3256764d3e01ed02064d35bffcf00aa
radx64 <radekteam@gmail.com>
2019-07-11 17:58:02 +0200

    Movable splitter between bookmarks and file view

--- ViewerWidget.cpp
+++ ViewerWidget.cpp
@@ -5,6 +5,7 @@
 #include <QSizePolicy>
 #include <QIcon>
 #include <QStandardItemModel>
+#include <QSplitter>
 
 #include "TabCompositeViewer.hpp"
 
@@ -15,20 +16,27 @@ ViewerWidget::ViewerWidget(QWidget* parent) : parent_(parent)
     bookmarks_->setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding));
     logViewer_ = new TabCompositeViewer(this);
     logViewer_->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding));
-    layout_->addWidget(bookmarks_);
-    layout_->addWidget(logViewer_);
+
+    QSplitter* splitter = new QSplitter();
+    splitter->addWidget(bookmarks_);
+    splitter->addWidget(logViewer_);
+
+    layout_->addWidget(splitter);
     this->setLayout(layout_);
 
     /* PoC mockup */
-    //bookmarks_->addItems(QStringList{"bookmark A","bookmark B"});
-
     QStandardItemModel* iStandardModel = new QStandardItemModel(this);
     QList<QStandardItem*>* items = new QList<QStandardItem*> ();
     QStandardItem* item = new QStandardItem();
     item->setText("Bookmark A");
-    item->setIcon(QIcon::fromTheme("appointment-new"));
+    item->setIcon(this->style()->standardIcon(QStyle::SP_DialogOpenButton));
     items->append(item);
+
+    item = new QStandardItem();
+    item->setText("Bookmark B");
+    item->setIcon(this->style()->standardIcon(QStyle::SP_DialogCloseButton));
+    items->append(item);
+
     iStandardModel->appendColumn(*items);
     bookmarks_->setModel(iStandardModel);
-
 }
